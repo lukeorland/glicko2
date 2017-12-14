9054444d1717d48aaa52b634d2d6b66fd4535aa6
Luke Orland <luke.orland@newsela.com>
2017-12-14 16:25:57 -0500

    Using rate_1vs1 repeatedly isn't the same as
    using Glicko2.rate() over a sequence of results,
    unless this code is wrong.
    
    ```
    actual: glicko2.Rating(mu=1803.107, phi=236.838, sigma=0.060)
    expected: glicko2.Rating(mu=1464.051, phi=151.515, sigma=0.060)
    ```

--- glicko2tests.py
+++ glicko2tests.py
@@ -37,3 +37,26 @@ def test_glickman_example():
     rated = env.rate(r1, [(WIN, r2), (LOSS, r3), (LOSS, r4)])
     # env.create_rating2(1464.06, 151.52, 0.05999)
     assert almost(rated) == env.create_rating(1464.051, 151.515, 0.05999)
+
+
+def test_glickman_another_way():
+    env = Glicko2(mu=1500, phi=350, sigma=0.06, tau=0.5)
+    r1 = env.create_rating(1500, 200, 0.06)
+    r2 = env.create_rating(1400, 30)
+    r3 = env.create_rating(1550, 100)
+    r4 = env.create_rating(1700, 300)
+
+    rated = r1
+    for result, opponent in [
+            (WIN, r2), (LOSS, r3), (LOSS, r4)
+    ]:
+        winner = rated if result == WIN else opponent
+        loser = rated if result == LOSS else opponent
+        rated, __ = env.rate_1vs1(winner, loser)
+
+    # env.create_rating2(1464.06, 151.52, 0.05999)
+    actual = almost(rated)
+    expected = env.create_rating(1464.051, 151.515, 0.05999)
+    print('actual:', actual)
+    print('expected:', expected)
+    assert actual == expected
